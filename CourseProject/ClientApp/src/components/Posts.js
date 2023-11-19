9fd621bc36945f952a9a0db240d78b41eca7d442
DESKTOP-STQVS2S\ilyap <ilya.popogoty@gmail.com>
2023-11-19 09:45:21 +0300

    1

--- CourseProject/ClientApp/src/components/Posts.js
+++ CourseProject/ClientApp/src/components/Posts.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+import {ModalButton} from './ModalBtn';
 
 const URL = '/api/posts';
+
 
 const Posts = () =>{
 
@@ -47,6 +48,25 @@ const addPost = async() =>{
     }
 }
 
+const updatePost = async(oldPost) =>{
+
+
+    const headers = new Headers();
+    headers.set('Content-Type', 'application/json');
+    const options = {
+        method: 'PATCH',
+        headers: headers,
+        body: JSON.stringify(oldPost)
+    };
+    const result = await fetch(URL, options);
+    if(result.ok){
+        const post = await result.json();
+        const updatedPost = allPosts.findIndex(x => x.id == oldPost.id);
+        allPosts[updatedPost] = post;
+        setPosts(allPosts.slice());
+    }
+}
+
     const deletePost = async (id) => {
         const options = {
             method: 'DELETE',
@@ -57,30 +77,6 @@ const addPost = async() =>{
         setPosts(allPosts.filter(x => x.id != id));
     }
 
-    const updatePost = async(oldPost) =>{
-
-        const headerFromUser = document.querySelector('#header').value;
-        const textFromUser = document.querySelector('#text').value;
-    
-        const newPost = {
-            header: headerFromUser,
-            text: textFromUser
-        };
-    
-        const headers = new Headers();
-        headers.set('Content-Type', 'application/json');
-        const options = {
-            method: 'POST',
-            headers: headers,
-            body: JSON.stringify(newPost)
-        };
-        const result = await fetch(URL, options);
-        if(result.ok){
-            const post = await result.json();
-            allPosts.push(post);
-            setPosts(allPosts.slice());
-        }
-    }
 
     useEffect(() => {
         getPosts();
@@ -99,11 +95,12 @@ const addPost = async() =>{
                  <button onClick = {() => addPost()}>Add post</button>
             </div>
             <div>
-                {allPosts.map(x => <PostItem key = {x.id} post = {x} deleteAction = {deletePost} updateAction={}/>)}
+                {allPosts.map(x => <PostItem key = {x.id} post = {x} deleteAction = {deletePost} updateAction={updatePost}/>)}
                 
             </div>
         </div>
     )
+    
     }
 
 export default Posts;
@@ -114,7 +111,23 @@ const PostItem = ({post, deleteAction, updateAction}) => {
         <div>
             <h2>{post.header}</h2>
             <p>{post.text}</p>
-            <button onClick={() => deleteAction(post.id)}>Delete</button>
-        </div>
+            <button onClick={() => deleteAction(post.id)}>Delete</button>        
+            <ModalButton buttonName={'Update'} title={'Update post'}
+                 modalContent={
+                    <div>
+                         <div style={{margin: '10px'}}>
+                            <input id="header" type="text" 
+                            defaultValue={post.header}
+                            onChange={x => post.header = x.target.value}/>
+                         </div>
+                         <div style={{margin: '10px'}}>
+                            <textarea id="text"  type="text" 
+                            defaultValue={post.text}
+                            onChange={x => post.text = x.target.value}/>
+                        </div>
+                         <button onClick = {() => updateAction(post)}>Update</button>
+                    </div>    
+                 }/>
+        </div>        
     )
 }
